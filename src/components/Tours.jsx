7d33ef93b1f300b507497262a0f08a17d571ef80
Monami <monami.saeki@gmail.com>
2025-02-05 14:03:33 +0200

    complere iteration 3

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,14 +1,16 @@
 import { tours } from "../data";
+import {useState} from 'react';
 import Title from "./Title";
 import Tour from "./Tour";
 
 const Tours = () => {
+  const [toursData, settoursData] = useState(tours);
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
-        {tours.map((tour) => {
+        {toursData.map((tour) => {
           return <Tour key={tour.id} {...tour} />;
         })}
       </div>
